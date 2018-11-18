478c673529247ee6ab9767d63e88124bd88ae1f8
soizic Lambert <soizic.lee@gmail.com>
2018-11-18 18:22:05 +0100

    ajout TDD 2

--- exercises/multiply.js
+++ exercises/multiply.js
@@ -14,14 +14,15 @@ let a;
 let b;
 
 function multiply (a,b) {
-
-    if (b>0) {
-        return (a +multiply(a,b-1));
-    }
-    else if (b<0) {
-
-        return -multiply(a,-b);
-    }
+    if(a===0) {
+        return 0;
+     }
+        if (b>0) {
+            return (a +multiply(a,b-1));
+         }
+            else if (b<0) {
+             return -multiply(a,-b);
+             }  
     return 0;
 }
 
